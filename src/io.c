f230010785dbe79f431c5ca4ae39d225edbf88e9
elliottrt <89316853+elliottrt@users.noreply.github.com>
2021-10-05 16:52:23 -0600

    removed error and warning functions

--- src/io.c
+++ src/io.c
@@ -11,7 +11,7 @@ char *load_file(char const *path, size_t *len)
 
     if (!file)
     {
-        error("IO", "Error opening file '%s'.", path);
+        exit(1);
     }
 
     fseek(file, 0, SEEK_END);
@@ -24,14 +24,14 @@ char *load_file(char const *path, size_t *len)
     {
         fclose(file);
         free(contents);
-        error("IO", "Error allocating memory for file '%s'.", path);
+        exit(1);
     }
 
     if( 1 != fread( contents , *len, 1 , file) )
     {
         fclose(file);
         free(contents);
-        error("IO", "Error reading contents of file '%s'.", path);
+        exit(1);
     }
     
     char *fixed_contents = calloc(sizeof(char), *len);
@@ -40,27 +40,6 @@ char *load_file(char const *path, size_t *len)
 
     fclose(file);
     return fixed_contents;
-}
-
-void error(char const *from, char const *msg, char const *option)
-{
-    warns(from, msg, option);
-    exit(1);
-}
-
-void errori(char const *from, char const *msg, int option)
-{
-    char buf[128];
-    sprintf(buf, msg, option);
-    printf("\n[%s]: %s\n", from, buf);
-    exit(1);
-}
-
-void warns(char const *from, char const *msg, char const *option)
-{
-    char buf[128];
-    sprintf(buf, msg, option);
-    printf("\n[%s]: %s\n", from, buf);
 }
 
 /* 
--- src/io.h
+++ src/io.h
@@ -5,10 +5,6 @@
 
 char *load_file(char const *path, size_t *len);
 
-void error(char const *from, char const *msg, char const *option);
-void errori(char const *from, char const *msg, int option);
-void warns(char const *from, char const *msg, char const *option);
-
 void printstril(char *str);
 void printchril(char chr);
 
